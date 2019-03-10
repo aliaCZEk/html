fdb1ac24196c8b1dc6763d471ad7e06dd3802aa5
Jakub Samek <jaksamek@gmail.com>
2019-03-10 23:32:20 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  switch (req.url) {
+    case "/nas":
+      res.writeHead(301, { Location: "https://kubiksamek.cz:50002" });
+      break;
+
+    case "/plex":
+      res.writeHead(301, { Location: "https://app.plex.tv" });
+      break;
+
+    case "/transmission":
+      res.writeHead(301, { Location: "http://kubiksamek.cz:50010" });
+      break;
+
+    case "/myjd":
+      res.writeHead(301, { Location: "https://my.jdownloader.org" });
+      break;
+
+    case "/makemkv":
+      res.writeHead(301, { Location: "https://kubiksamek.cz/50030" });
+      break;
+
+    case "/mkvtoolnix":
+      res.writeHead(301, { Location: "https://kubiksamek.cz/50040" });
+      break;
+
+    case "/jdownloader":
+      res.writeHead(301, { Location: "https://kubiksamek.cz/50050" });
+      break;
+
+    case "/homebridge":
+      res.writeHead(301, { Location: "http://kubiksamek.cz:50060" });
+      break;
+
+    case "/grafana":
+      res.writeHead(301, { Location: "http://kubiksamek.cz:50070" });
+      break;
+
+    default:
+      res.writeHead(404);
+      break;
+  }
+  res.end();
+});
+
+server.listen(80);
